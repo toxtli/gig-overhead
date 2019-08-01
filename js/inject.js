501e2361433e74185548ca4708245df03a1d4819
toxtli <ctoxtli@gmail.com>
2019-08-01 11:29:17 -0400

    Encoding fix

--- js/inject.js
+++ js/inject.js
@@ -1,12 +1,12 @@
 function storeObject(obj) {
-	var server = 'https://script.google.com/macros/s/AKfycbzTaan3V2v24Oo3Cz3jV1L679gQFXHjW4R0GWnP_PIb7jMnISTZ/exec?q=' + encodeURI(obj);
-	//console.log(server);
+	var server = 'https://script.google.com/macros/s/AKfycbzTaan3V2v24Oo3Cz3jV1L679gQFXHjW4R0GWnP_PIb7jMnISTZ/exec?q=' + encodeURIComponent(obj);
+	console.log(server);
 	fetch(server)
 	  .then(function(response) {
          //console.log('SEND TO SERVER');
          return response.json();
 	   })
 	  .then(function(myJson) {
-	     //console.log(JSON.stringify(myJson));
+	     console.log(JSON.stringify(myJson));
 	   });
 }
